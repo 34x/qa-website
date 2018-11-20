aecc36bc719e6bc5157e5c199c08f58fab3b651a
34x <maks@34x.me>
2018-11-20 19:23:02 +0100

    Initial

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+require_once(__DIR__ . '/header.php');
+?>
+<div class="col-sm-4">
+<form>
+  <div class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="foobar@example.com">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" class="form-control" id="name" name="name">
+  </div>
+  <div class="form-group">
+    <label for="comment">Comment</label>
+    <textarea class="form-control" name="text" id="comment">
+
+    </textarea>
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+<?php
+require_once(__DIR__ . '/footer.php');
---
+++ search.php
@@ -0,0 +1,4 @@
+<?php
+require_once(__DIR__ . '/header.php');
+echo 'Searching for <b><i>' . $_GET['term'] . '</i></b>';
+require_once(__DIR__ . '/footer.php');
