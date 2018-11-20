29b4d7809b140412e5960a5d89281dab04a2eb64
34x <maks@34x.me>
2018-11-20 21:29:09 +0100

    Experimenting with db

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+use AppBundle\Entity\Post;
+
 class DefaultController extends Controller
 {
     /**
@@ -13,6 +15,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $repository = $this->getDoctrine()->getRepository(Post::class);
+
+        $products = $repository->findAll();
+
+        var_dump($products);die();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
